422a0fe7a9e65d75c165022499c49c835412fb68
Luis Fernando Machado <>
2023-02-16 22:34:04 -0300

    Using toolbar and applying menu in context of application

--- app/src/main/java/com/materialdesign/collapsinglayout/MainActivity.java
+++ app/src/main/java/com/materialdesign/collapsinglayout/MainActivity.java
@@ -1,14 +1,28 @@
 package com.materialdesign.collapsinglayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+	private Toolbar toolbar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		//return super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.menu_personalized, menu);
+		return true;
 	}
 }
